e88c8222c0c9f173a712e006c700742d80992ad4
sergblin <blinofftash86@mail.ru>
2022-12-28 10:45:50 +0300

    перебор массива и нахождение нужных элементов

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] strArray = {"Hello", "2", "world", ":-)"};
 string[] newStrArray = new string[strArray.Length];
+void ModifiedStringArray(string[] strArray, string[] newStrArray)
+{
+    int count = 0;
+    for(int i = 0; i < strArray.Length; i++)
+    {
+        if(strArray[i].Length <= 3)
+        {
+            newStrArray[count] = strArray[i];
+            count++;
+        }
+    }
+}
+
